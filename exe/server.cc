a3810fcdf726c7e25d70f084923e026a7c3e0edb
Felix Gündling <felix.guendling@gmail.com>
2024-07-11 09:08:18 +0200

    print server

--- exe/server.cc
+++ exe/server.cc
@@ -1,10 +1,13 @@
 #include "net/run.h"
 #include "net/web_server/query_router.h"
 #include "net/web_server/web_server.h"
+#include "net/web_server/responses.h"
 
 #include "boost/program_options.hpp"
 
 namespace asio = boost::asio;
+using net::web_server;
+using namespace net;
 using namespace std::string_literals;
 
 constexpr auto const response =
@@ -17,7 +20,7 @@ int main(int ac, char** av) {
   namespace bpo = boost::program_options;
 
   auto server_ip = "0.0.0.0"s;
-  auto server_port = "8080"s;
+  auto server_port = "80"s;
   auto client_ip = "0.0.0.0"s;
   auto client_port = "80"s;
 
@@ -42,9 +45,15 @@ int main(int ac, char** av) {
 
   auto ioc = asio::io_context{};
   auto s = net::web_server{ioc};
-  auto qr = net::query_router{}.route("*", ".*", [](std::string_view body) {
-    std::cout << "BODY: " << body << "\n\n";
-    return std::string{response};
+  auto qr = net::query_router{}.route("*", ".*", [](route_request const& req, web_server::http_res_cb_t res_cb, bool) {
+    std::cout << "REQ: " << req.base().method() << ", " << req.base().target() << "\n";
+    std::cout << "HEADERS:\n";
+    for (auto const& h : req.base()) {
+      std::cout << h.name() << ": " << h.value() << "\n";
+    }
+    std::cout << "BODY: " << req.body() << "\n\n";
+    std::cout << std::flush;
+    res_cb(server_error_response(req, "not implemented yet"));
   });
   qr.enable_cors();
   s.on_http_request(std::move(qr));
